083ee394c8031fdbbd14da83677d906b3a4cdf70
Laurens van der BIlt <54841481+2099492@users.noreply.github.com>
2020-06-26 12:14:41 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-//Required for the bot
 require('dotenv').config();
 const token = process.env.SLACK_TOKEN;
 const mysql = require('mysql');
-var SlackBot = require('slackbots');
 
 var connection = mysql.createConnection({
 	host: "skil1.mysql.database.azure.com",
@@ -17,14 +15,16 @@ connection.connect(function (err) {
 	console.log("Connected!");
 });
 
+var SlackBot = require('slackbots');
+const coaches = ["UF47HSVPT", "UQEB1731P", "UQCSZPV1U", "UQAM223S9"];
+const masters = ["UQCGKUNAK"];
 
-const coaches = ["UQCGKUNAK", "UF47HSVPT", "UQEB1731P", "UQCSZPV1U", "UQAM223S9"];
-
-// Create a bot
+// create a bot
 var bot = new SlackBot({
-	token: process.env.SLACK_TOKEN, 
+	token: process.env.SLACK_TOKEN, // Add a bot https://my.slack.com/services/new/bot and put the token
 	name: 'skilbot'
 });
+
 bot.on('start', () => {
 	const params = {
 		icon_emoji: ':robot:'
@@ -37,6 +37,7 @@ bot.on('start', () => {
 	);
 });
 
+//message Handler
 bot.on('message', (message) => {
 	if (message.type == 'message') {
 		console.log(message.user);
@@ -45,7 +46,13 @@ bot.on('message', (message) => {
       if (coaches.includes(message.user)) {
         console.log('test coaches includes');
         issuesTies();
+      } else {
+      	console.log('error');
       }
+
+			if (masters.includes(message.user)) {
+				deleteIssue();
+			}
 	}
 });
 
@@ -58,7 +65,7 @@ bot.on('error', err => console.log(err));
 //functions
 function issuesTies() {
 	console.log('begin functie issuesTies()');
-	connection.query("SELECT idIssue FROM coachIssue WHERE idCoach=1", function STUDENTS(err, resultTies) {
+	connection.query("SELECT idIssue FROM coachIssue", function STUDENTS(err, resultTies) {
 		for (var i = 0; i < resultTies.length; i++) {
 			console.log(`index: ${resultTies[i].idIssue} in de for loop`);
 			var ID = resultTies[i].idIssue;
@@ -69,10 +76,29 @@ function issuesTies() {
 				for (var j = 0; j < resultIssue.length; j++) {
 					bot.postMessageToChannel(
 						'skilbot_test_channel',
-						resultIssue[j]
+						`Naam: (${resultIssue[j].naam})   Issue: (${resultIssue[j].issue})   Date: (${resultIssue[j].arrivalDate})   ID: (${resultIssue[j].ID})`
 					);
 				}
 			});
 		}
 	});
+}
+
+function deleteIssue(message) {
+	console.log(message);
+	var Id = message;
+	var sql   = 'DELETE FROM coachIssue WHERE idIssue  = ' + connection.escape(Id);
+	connection.query(sql, function (error, results, fields) {
+	  if (error) throw error;
+	console.log('deleted ' + results.affectedRows + ' rows');
+	});
+}
+
+
+function insert(message) {
+	var sql = `INSERT INTO issues (naam, issue) VALUES (${message}, ${message})`;
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
 }
