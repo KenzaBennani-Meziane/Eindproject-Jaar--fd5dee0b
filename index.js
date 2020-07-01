200f0157b80b785899b0b2cec5cae0a12bda2c70
Laurens van der BIlt <54841481+2099492@users.noreply.github.com>
2020-07-01 14:18:58 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ pool.getConnection(function(err, connection) {
 });
 
 var SlackBot = require('slackbots');
-const coaches = ["UQCGKUNAK", "UF47HSVPT", "UQEB1731P", "UQCSZPV1U", "UQAM223S9"];
+const coaches = ["UQCGKUNAK", "UF47HSVPT", "UQEB1731P", "UQCSZPV1U", "UQAM223S9", "UFYU97CSU", "UTCT9JF4H"];
 const masters = ["UQCGKUNAK"];
 
-// create a bot
+// Create a bot
 var bot = new SlackBot({
 	token: process.env.SLACK_TOKEN, // Add a bot https://my.slack.com/services/new/bot and put the token
 	name: 'skilbot'
@@ -34,7 +34,7 @@ bot.on('start', () => {
   console.log("SKILbot to je beschikking")
 });
 
-//message Handler
+// Message Handler
 bot.on('message', (message) => {
 });
 
@@ -43,10 +43,10 @@ bot.on('message', (message) => {
 // Error Handler
 bot.on('error', err => console.log(err));
 
-// /commands
+// Slash Commands
 app.post('/issues', (req, res) => {
   console.log(req.body.text);
-  console.log(req.body.user_id);
+  console.log(req.body.user_name + " executed /issues");
   pool.query("SELECT * FROM ISSUES ORDER BY ID ASC", function ISSUES(err, resultIssue) {
     if (err) throw err;
       for (var j = 0; j < resultIssue.length; j++) {
@@ -58,27 +58,74 @@ app.post('/issues', (req, res) => {
   });
 });
 
-app.post('/delete', (req, res) => {
-  var Id = req.body.text;
-  function deleteIssue() {
-    var sql = 'DELETE FROM ISSUES WHERE ID  = ' + pool.escape(Id);
-    pool.query(sql, function (error, results, fields) {
-      if (error) throw error;
-    res.send('deleted ' + results.affectedRows + ' rows');
-    });
-  }
-  deleteIssue();
+app.post('/delete-issue', (req, res) => {
+  var Text = req.body.text;
+  var userName = req.body.user_name;
+  console.log(req.body.user_name + " executed /delete-issue");
+	if(coaches.includes(req.body.user_id)){
+    function deleteIssue() {
+      var sql = 'DELETE FROM ISSUES WHERE ID  = ' + pool.escape(Text);
+      pool.query(sql, function (error, results, fields) {
+        if (error) throw error;
+        res.send('deleted ' + results.affectedRows + ' rows');
+  	  });
+    }
+ }
+ bot.postMessageToChannel(
+   "_skillbot_test_public",
+   `ISSUE SOLVED BY: ${userName}`
+ );
+ deleteIssue();
 });
 
-app.post('/add', (req, res) => {
-  var Id = req.body.text;
+app.post('/add-issue', (req, res) => {
+  var Text = req.body.text;
+  var userName = req.body.user_name;
+  console.log(req.body.user_name + " executed /add-issue");
   function insert() {
-  	var sql = `INSERT INTO issues (naam, issue) VALUES ('Laurens', '${Id}')`;
+  	var sql = `INSERT INTO issues (naam, issue) VALUES ('${userName}', '${Text}')`;
     pool.query(sql, function (err, result) {
       if (err) throw err;
       res.send("1 record inserted");
     });
   }
+  bot.postMessageToChannel(
+    "_skillbot_test_public",
+    `NEW ISSUE ADDED BY: ${userName}`
+  );
   insert();
+});
+
+app.post('/skil-info', (req, res) => {
+  console.log(req.body.user_name + " executed /skil-info");
+    res.header('Content-Type', 'application/json');
+    res.write(JSON.stringify({
+    "blocks": [
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": ":slack: Hi, ik ben SKILbot!\nIk ben op dit platform gezet door drie studenten van de Bit Academy, maar dit gaat niet over mij, maar over wat ik kan.  \n \n Hier vind je een lijst van alle commands: \n*/skil-info* — _De command die je zojuist hebt gebruikt om dit te zien._ \n*/issues* — _Alle issues vertonen._ \n*/add* — _Een issue toevoegen in de queue._ \n*/delete* — _Een issue verwijderen van de queue._ \n\n\n\n\n\n\nVragen? Ik ben helaas _niet_ geprogammeerd die te beantwoorden, maar neem gerust contact op met *Iz-Dine*, *Kenza* of *Laurens*!"
+            },
+            "accessory": {
+                "type": "image",
+                "image_url": "https://avatars3.githubusercontent.com/u/53334549?s=280&v=4",
+                "alt_text": "Let's code!"
+            }
+        },
+        {
+            "type": "context",
+            "elements": [
+                {
+                    "type": "plain_text",
+                    "text": " SKILbot© created by Iz-Dine, Kenza and Laurens.",
+                    "emoji": true
+                }
+            ]
+        }
+    ]
+}
+    ));
+    res.end();
 });
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
